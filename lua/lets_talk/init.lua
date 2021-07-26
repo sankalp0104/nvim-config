bee49d97421dfba12c53d3aaba5ecdb01bc6d9f0
sankalp0104 <sankalp0104@hotmail.com>
2021-07-27 04:53:54 +0530

    Cleanup

--- lua/lets_talk/init.lua
+++ lua/lets_talk/init.lua
@@ -1,11 +1,5 @@
 require("lets_talk.telescope")
 
--- local on_attach_vim = function(client)
---     require'completion'.on_attach(client)
--- end
-
--- require'lspconfig'.clangd.setup{ on_attach=on_attach_vim }
-
 require('nvim-treesitter.configs').setup { 
     highlight = { 
         enable = true,
